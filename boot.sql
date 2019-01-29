9970388e3ea91e93ab509590aee20e2395154c37
fanyu <fanyu4j@gmail.com>
2019-01-29 10:10:26 +0800

    t_order

--- boot.sql
+++ boot.sql
@@ -11,6 +11,21 @@ CREATE TABLE `t_user` (
 ) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8mb4;
 
 -- ----------------------------
+-- Table structure for `t_order`
+-- ----------------------------
+CREATE TABLE `order` (
+  `id` bigint(20) NOT NULL AUTO_INCREMENT,
+  `money` DOUBLE ,
+  `receiverAddress` VARCHAR(255) ,
+  `receiverName` VARCHAR(20) ,
+  `receiverPhone` VARCHAR(20) ,
+  `paystate` INT(11) ,
+  `ordertime` TIMESTAMP ,
+  `user_id` INT(11) ,
+  PRIMARY KEY (`id`)
+)
+
+-- ----------------------------
 -- Records of t_user
 -- ----------------------------
 INSERT INTO `t_user` VALUES ('1', 'fanyu', '30', '北京');
